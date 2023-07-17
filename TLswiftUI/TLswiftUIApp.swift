270a7610bc48138d2c75743a2f76ee5f52a95d68
soloexploder <soloexploder@gmail.com>
2023-07-17 17:03:42 +0300

    Initial Commit

---
+++ TLswiftUI/TLswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TLswiftUIApp.swift
+//  TLswiftUI
+//
+//  Created by Han Solo on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TLswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
